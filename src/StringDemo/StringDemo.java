5c72d452a1e040df4147ae9457c1ebdc26fe7e64
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-06-15 08:31:25 +0530

    Add StringDemo

---
+++ src/StringDemo/StringDemo.java
@@ -0,0 +1,24 @@
+package StringDemo;
+
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String username = "Tendulkar";
+        int size = username.length(); // returns the length of string
+        if (size > 8 && size < 15) {
+            char arr[] = username.toCharArray();// converts the string to character array
+            int count = 0;
+            for (char c : arr) {
+                if (Character.isLetter(c)) {
+                    ++count;
+                }
+            }
+            if (count == size) {
+                System.out.println("valid username");
+            }
+        }
+    }
+}
+
+
+
