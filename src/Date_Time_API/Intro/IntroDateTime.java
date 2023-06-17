4ba15b45819f3b3f5413ce4f1c44551c2386b753
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-06-17 09:40:34 +0530

    Add IntroDateTime

---
+++ src/Date_Time_API/Intro/IntroDateTime.java
@@ -0,0 +1,19 @@
+package Date_Time_API.Intro;
+
+import java.time.LocalDate;
+
+public class IntroDateTime {
+    public static void main(String[] args) {
+        // Current Date
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date: " + date);
+
+        // Yesterday
+        LocalDate yesterday = date.minusDays(1);
+        System.out.println("Yesterday: " + yesterday);
+
+        // Tomorrow
+        LocalDate tomorrow = date.plusDays(1);
+        System.out.println("Tomorrow: " + tomorrow);
+    }
+}
